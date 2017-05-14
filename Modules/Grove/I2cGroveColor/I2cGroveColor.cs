1cc2fa68d4d647a06996dd6e11c8522974d98f09
Nicolas Bauland <nicolas.bauland@laposte.net>
2017-05-14 15:15:00 +0200

    Début de l'implémentation du capteur de couleurs

--- Modules/Grove/I2cGroveColor/I2cGroveColor.cs
+++ Modules/Grove/I2cGroveColor/I2cGroveColor.cs
@@ -1,37 +1,51 @@
 ﻿using System;
-using System.Collections;
 using System.Diagnostics;
 using System.Drawing;
-using System.Text;
 using System.Threading;
 using GHIElectronics.TinyCLR.Devices.Enumeration;
 using GHIElectronics.TinyCLR.Devices.I2c;
 
 namespace I2cGroveColor
 {
+    // ReSharper disable once InconsistentNaming
     public class I2cGroveColor
     {
         private const byte DeviceAddress = 0x39;
-        private const byte RegistryBlockRead = 0xd0;
+        private int _red, _green, _blue;
+        private int _clear;
+
+        public bool LedStatus { get; set; }
+
 
         private I2cDevice _i2c;
 
+        private byte CommandRegister(Register reg, Transaction tr)
+        {
+            int result = 0x80;
+
+            result += (int)reg;
+            result += (int)tr;
+            return (byte)result;
+
+        }
+
+        // ReSharper disable once InconsistentNaming
         public I2cGroveColor(string i2c)
         {
             var devices = DeviceInformation.FindAll(i2c);
             I2cConnectionSettings ics = new I2cConnectionSettings(DeviceAddress) { BusSpeed = I2cBusSpeed.FastMode, SharingMode = I2cSharingMode.Shared };
             _i2c = I2cDevice.FromId(devices[0].Id, ics);
-            WriteReg(0x80, 0x03);
+            WriteReg(Register.Control, 0x03);
         }
 
-        public Color GetRgbColor()
+        public Color Test()
         {
             Color color = new Color();
 
-            Debug.WriteLine("## ID: 0x" + ReadReg(0x84).ToString("X"));
-            Debug.WriteLine("## CTL: 0x" + ReadReg(0x80).ToString("X"));
+            Debug.WriteLine("## ID: 0x" + ReadReg(Register.ID).ToString("X"));
+            Debug.WriteLine("## CTL: 0x" + ReadReg(Register.Control).ToString("X"));
             Thread.Sleep(15);
-            Debug.WriteLine("## CTL: 0x" + ReadReg(0x80).ToString("X"));
+            Debug.WriteLine("## CTL: 0x" + ReadReg(Register.Control).ToString("X"));
             //ReadByte(Address, Command, DataLow) //Result returned in DataLow
             //Command = 0x91 //Address the Ch1 upper data register
             //ReadByte(Address, Command, DataHigh) //Result returned in DataHigh
@@ -54,10 +68,33 @@ namespace I2cGroveColor
             return color;
         }
 
-        private byte ReadReg(byte reg)
+        public byte[] GetARGB()
         {
+            byte[] data = ReadBlock(Register.Data1High);
+            _green = data[1] * 256 + data[0];
+            _red = data[3] * 256 + data[2];
+            _blue = data[5] * 256 + data[4];
+            _clear = data[7] * 256 + data[6];
+            _red = Normalize(_red);
+            _red = (int)((_red - 7.0) * 255.0 / 87.0);
+            _green = Normalize(_green);
+            _green = (int)((_green - 7.0) * 255.0 / 89.0);
+            _blue = Normalize(_blue);
+            _blue = (int)((_blue - 30.0) * 255.0 / 64.0);
+            // Normalize
+            return new byte[] { (byte)(_red), (byte)(_green), (byte)(_blue), Normalize(_clear) };
+        }
+
+        private byte Normalize(int valueToNormalize)
+        {
+            return (byte)(valueToNormalize * 255.0 / 65535.0);
+        }
+
+        private byte ReadReg(Register reg)
+        {
+            Debug.WriteLine("## Read register: " + reg);
             byte[] writeBuffer = new byte[1];
-            writeBuffer[0] = reg;
+            writeBuffer[0] = CommandRegister(reg, Transaction.Byte);
             byte[] readBuffer = new byte[1];
             var tr = _i2c.WritePartial(writeBuffer);
             Debug.WriteLine("Write #: " + tr.BytesTransferred + ", :" + (tr.Status == I2cTransferStatus.FullTransfer ? "OK" : "Erreur") + " , :0x" + writeBuffer[0].ToString("X"));
@@ -65,9 +102,24 @@ namespace I2cGroveColor
             Debug.WriteLine("Read #: " + tr.BytesTransferred + ", :" + (tr.Status == I2cTransferStatus.FullTransfer ? "OK" : "Erreur") + " , :0x" + readBuffer[0].ToString("X"));
             return readBuffer[0];
         }
-        private void WriteReg(byte reg, byte data)
+
+        private byte[] ReadBlock(Register reg)
         {
-            byte[] writeBuffer = new byte[] { reg, data };
+            Debug.WriteLine("## Read register: " + reg);
+            byte[] writeBuffer = new byte[1];
+            writeBuffer[0] = CommandRegister(reg, Transaction.Block);
+            byte[] readBuffer = new byte[9];
+            var tr = _i2c.WritePartial(writeBuffer);
+            Debug.WriteLine("Write #: " + tr.BytesTransferred + ", :" + (tr.Status == I2cTransferStatus.FullTransfer ? "OK" : "Erreur") + " , :0x" + writeBuffer[0].ToString("X"));
+            tr = _i2c.ReadPartial(readBuffer);
+            Debug.WriteLine("Read #: " + tr.BytesTransferred + ", :" + (tr.Status == I2cTransferStatus.FullTransfer ? "OK" : "Erreur") + " , :0x" + readBuffer[0].ToString("X"));
+            return readBuffer;
+        }
+
+        private void WriteReg(Register reg, byte data)
+        {
+            Debug.WriteLine("## write 0x" + data.ToString("X") + " to register: " + reg);
+            byte[] writeBuffer = new byte[] { CommandRegister(reg, Transaction.Byte), data };
             var tr = _i2c.WritePartial(writeBuffer);
             Debug.WriteLine("Write #: " + tr.BytesTransferred + ", :" + (tr.Status == I2cTransferStatus.FullTransfer ? "OK" : "Erreur") + " , :0x" + writeBuffer[0].ToString("X"));
         }
--- Tests/TestG80/MyG80App.cs
+++ Tests/TestG80/MyG80App.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Drawing;
+using System.Threading;
 using GHIElectronics.TinyCLR.Pins;
 using TinyClrCore;
 
@@ -7,23 +8,29 @@ namespace TestG80
 {
     internal class MyG80App : Application
     {
+        private I2cGroveColor.I2cGroveColor module;
+
         public override void UpdateLoop()
         {
+            byte[] data = module.GetARGB();
+            Debug.WriteLine("Color R:" + data[0] + ", G:" + data[1] + ", B:" + data[2]);
+            Thread.Sleep(1000);
         }
 
         public override void ProgramStarted()
         {
-            Debug.WriteLine("Scanning ...");
-            foreach (var value in I2CScanner.Ping(FEZPandaIII.I2cBus.I2c1))
-            {
-                Debug.WriteLine(((int)value).ToString("X"));
-            }
-            Debug.WriteLine("End scanning.");
+            //Debug.WriteLine("Scanning ...");
+            //foreach (var value in I2CScanner.Ping(FEZPandaIII.I2cBus.I2c1))
+            //{
+            //    Debug.WriteLine(((int)value).ToString("X"));
+            //}
+            //Debug.WriteLine("End scanning.");
 
-            I2cGroveColor.I2cGroveColor module=new I2cGroveColor.I2cGroveColor(FEZPandaIII.I2cBus.I2c1);
-            Color color =module.GetRgbColor();
+            module = new I2cGroveColor.I2cGroveColor(FEZPandaIII.I2cBus.I2c1);
             Debug.WriteLine("Module initialized.");
-
+            module.LedStatus = false;
+            byte[] data = module.GetARGB();
+            Debug.WriteLine("Color R:" + data[0] + ", G:" + data[1] + ", B:" + data[2]);
         }
     }
 }
