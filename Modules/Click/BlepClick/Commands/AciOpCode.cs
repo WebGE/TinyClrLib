bfad179e59312ff8b9c55dbfb9ae8c179bf273a3
Nicolas Bauland <nicolas.bauland@laposte.net>
2017-05-27 22:20:42 +0200

    NOT COMPLETED Ajout de code pour nrf8001 Ajout du code de test pour Brainpad

---
+++ Modules/Click/BlepClick/Commands/AciOpCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Threading;
+
+namespace BlepClick.Commands
+{
+    public enum AciOpCode:byte
+    {
+        Test=0x01,
+        Echo=0x02,
+        Sleep=0x04,
+        WakeUp=0x05,
+        Setup=0x06,
+        Connect=0x0f,
+        Bond=0x10,
+        OpenRemotePipe=0x14,
+        SendData=0x15
+    }
+}
