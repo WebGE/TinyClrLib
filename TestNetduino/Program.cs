b701abf9167062c0b7b6f4588dc16c14ce6be2eb
Nicolas Bauland <nicolas.bauland@laposte.net>
2017-08-21 11:10:39 +0200

    bug: TestBrainPad can't be loaded

---
+++ TestNetduino/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Threading;
+
+namespace TestNetduino
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
