51b9f7c74a1555cc008e8365ecf5b62826863b7a
Nicolas Bauland <nicolas.bauland@laposte.net>
2017-11-05 10:13:56 +0100

    Suppression of all samples
    Create only one to test modules

---
+++ Tests/testUniversal/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using GHIElectronics.TinyCLR.Devices.Gpio;
+
+namespace testUniversal
+{
+    static class Program
+    {
+        private static GpioPin  _led;
+        static void Main()
+        {
+            switch (DeviceInformation.DeviceName)
+            {
+                case "FEZ":
+                    SetupFez();
+                    break;
+                default:
+                    Debug.WriteLine("Unkown board: " + DeviceInformation.DeviceName);
+                    throw new Exception("Unkown board: " + DeviceInformation.DeviceName);
+            }
+            while (true)
+            {
+                _led.Write(GpioPinValue.High);
+                Thread.Sleep(200);
+                _led.Write(GpioPinValue.Low);
+                Thread.Sleep(800);
+            }
+        }
+
+        private static void SetupFez()
+        {
+            _led = GpioController.GetDefault().OpenPin(1);
+            _led.SetDriveMode(GpioPinDriveMode.Output);
+        }
+    }
+}
