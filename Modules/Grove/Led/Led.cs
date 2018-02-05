28159eff96cba6f8bdbe46a039089f3c06cce0b8
Nicolas Bauland <nicolas.bauland@laposte.net>
2018-02-05 22:27:07 +0100

    TIN-61 Add Grove Led module

---
+++ Modules/Grove/Led/Led.cs
@@ -0,0 +1,78 @@
+﻿using System.Threading;
+using GHIElectronics.TinyCLR.Devices.Gpio;
+// ReSharper disable UnusedMember.Global
+
+namespace Bauland.Grove
+{
+    /// <summary>
+    /// Wrapper for Led Grove module
+    /// </summary>
+    public class Led
+    {
+
+        private readonly GpioPin _pin;
+        private readonly Thread _blinkerThread;
+        private int _blinkDelay = 300;
+        private void Blinker()
+        {
+            while (true)
+            {
+                _pin.Write(GpioPinValue.Low);
+                Thread.Sleep(_blinkDelay);
+                _pin.Write(GpioPinValue.High);
+                Thread.Sleep(_blinkDelay);
+            }
+            // ReSharper disable once FunctionNeverReturns
+        }
+
+        /// <summary>
+        /// Constructor of Led
+        /// </summary>
+        /// <param name="gpioPinNumber">Pin number on which led is connected.</param>
+        public Led(int gpioPinNumber)
+        {
+            _pin = GpioController.GetDefault().OpenPin(gpioPinNumber);
+            _pin.Write(GpioPinValue.Low);
+            _pin.SetDriveMode(GpioPinDriveMode.Output);
+            _blinkerThread = new Thread(Blinker);
+            _blinkerThread.Start();
+            _blinkerThread.Suspend();
+        }
+
+        /// <summary>
+        /// Turn the Led on
+        /// </summary>
+        public void TurnOn()
+        {
+            lock (_blinkerThread)
+            {
+                _blinkerThread.Suspend();
+                _pin.Write(GpioPinValue.High);
+            }
+        }
+
+        /// <summary>
+        /// Turn the Led off
+        /// </summary>
+        public void TurnOff()
+        {
+            lock (_blinkerThread)
+            {
+                _blinkerThread.Suspend();
+                _pin.Write(GpioPinValue.Low);
+            }
+        }
+        /// <summary>
+        /// Make the Led blinking
+        /// </summary>
+        /// <param name="blinkRateSec">Rate of blinking in second.</param>
+        public void Blink(double blinkRateSec = 3)
+        {
+            _blinkDelay = (int)(((1 / blinkRateSec) * 1000) / 2);
+            lock (_blinkerThread)
+            {
+                _blinkerThread.Resume();
+            }
+        }
+    }
+}
---
+++ Modules/Grove/Led/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Grove Led")]
+[assembly: AssemblyDescription("Wrapper for Grove Led")]
+[assembly: AssemblyProduct("Led")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c3455f34-4114-483a-978e-1ebe8a6c5650")]
