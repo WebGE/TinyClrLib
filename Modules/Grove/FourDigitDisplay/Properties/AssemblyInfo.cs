cf27747a7b5b6ccd585ac9125de10867923bcfaf
Nicolas Bauland <nicolas.bauland@laposte.net>
2018-02-08 21:20:28 +0100

    TIN-63 Add Grove FourDigitDisplay module

---
+++ Modules/Grove/FourDigitDisplay/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Grove FourDigitDisplay")]
+[assembly: AssemblyDescription("Wrapper for Grove FourDigitDisplay")]
+[assembly: AssemblyProduct("FourDigitDisplay")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7b18251e-2072-47ee-8194-0a2bec2b9771")]
